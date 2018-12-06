497b48eff2d01aca67330d5f6cdc0255093dcd77
programer10 <programer10@ucione.local>
2018-12-06 21:27:04 +0100

    Login modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,42 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+
+var handleLogin = function () {
+
+    var loginForm = $('#loginForm');
+
+    loginForm.on('submit', function (e) {
+
+        e.preventDefault();
+
+        $(this).parsley({
+
+            errorsWrapper: '<div class="text-danger"></div>',
+            errorTemplate: '<span></span>'
+
+        }).validate();
+
+        if ($(this).parsley().isValid()) {
+
+            var data = $(this).serialize();
+
+            console.log(data);
+
+        }
+
+    });
+
+};
+
+var App = function () {
+
+    return {
+
+        init: function () {
+
+            handleLogin();
+
+        }
+
+    };
+
+}();
 
